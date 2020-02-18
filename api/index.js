ed1ceaa67f39157c9b16a3d1aa1f51c5f22be8c6
Christopher Miller <admin@mctherealm.net>
2020-02-18 17:51:18 -0500

    Began work on package json organization for front and back

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
