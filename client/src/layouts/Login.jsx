09c580e103beb873fa4720f79a61bf3b71191cdf
Dhruv Patel <dhruvpatel1@ufl.edu>
2020-02-24 17:24:01 -0500

    Not actual commit

--- client/src/layouts/Login.jsx
+++ client/src/layouts/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Page from '../components/Page';
+import { Button } from '@material-ui/core';
 
 const Login = () => {
   return (
     <Page>
+      <Button variant="contained" color="secondary">BOI</Button> 
       <div>Login</div>
     </Page>
   );
