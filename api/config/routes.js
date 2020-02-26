f5f9b8532087d4241b4edb5984fedb71d3ad0431
alenjames2000 <alenjames@ufl.edu>
2020-02-25 20:57:53 -0500

    userController and routes for get/delete

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,6 +1,11 @@
+import * as users from '../controllers/userController.js';
 
 const router = (app) => {
   app.get('/api', (req, res) => res.send('Hello World!'));
+
+  //User Routes
+  app.get('/users/:someId', users.read);
+  app.delete('/users/:someId', users.remove);
 };
 
 export default router;
---
+++ api/controllers/userController.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import User from '../model/user.js';
+
+export const read = (req, res) => {
+//Find User from Database and return
+};
+
+export const remove = (req, res) => {
+//Find User from Database and remove
+};
