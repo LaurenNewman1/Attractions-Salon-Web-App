acb2e6452b58de08f3958332a85a45b415a934e9
alenjames2000 <alenjames@ufl.edu>
2020-02-26 20:47:17 -0500

    Updates to userController(get/remove)

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,8 +3,34 @@ import User from '../model/user.js';
 
 export const read = (req, res) => {
 //Find User from Database and return
+    User.findById(req.params.someId, (err, data) => {
+        if(err){
+            console.log(err);
+        }
+        else if(data == null){
+            data = {error: 'User not found!'};
+            res.send(data);
+        }
+        else{
+            res.send(data);
+        }
+    });
 };
 
 export const remove = (req, res) => {
 //Find User from Database and remove
+    User.findByIdAndRemove(req.params.someId, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else if(!data) {
+            data = {error: 'User not found!'};
+            res.send(data);
+        }
+        else{
+            data = {error: 'User deleted'};
+            res.send(data);
+        }
+
+    });
 };
