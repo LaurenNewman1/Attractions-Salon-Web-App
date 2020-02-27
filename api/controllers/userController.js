6999559034ff5d5ba153017fc73f6bde40f00993
ruiz-m <ruiz.m@ufl.edu>
2020-02-26 21:21:32 -0500

    userController(get/remove/update)

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,14 +1,57 @@
 import mongoose from 'mongoose';
 import User from '../model/user.js';
+import * as argon2 from "argon2";
 
 export const read = (req, res) => {
 //Find User from Database and return
+    User.findById(req.params.someId, (err, data) => {
+        if(err){
+            console.log(err);
+        }
+        else if(data == null){
+            data = {error: 'User not found!'};
+            res.send(data);
+        }
+        else{
+            res.send(data);
+        }
+    });
 };
 
 export const remove = (req, res) => {
 //Find User from Database and remove
+    User.findByIdAndRemove(req.params.someId, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else if(!data) {
+            data = {error: 'User not found!'};
+            res.send(data);
+        }
+        else{
+            data = {error: 'User deleted'};
+            res.send(data);
+        }
+
+    });
 };
 
 export const update = (req, res) => {
-//Find User from Database and update fields
+    User.findById(req.params.someId, (err, data) => {
+        if(err){
+            res.status(404).send(err);
+        }
+        data.name = req.body.name;
+        data.email = req.body.email;
+        data.phone_number = req.body.phone_number;
+        if(!req.body.password) {
+            //check if inserted password is equivalent to the current password for security purposes
+            const hash = await argon2.hash(req.params.inserted_password);
+            const valid = await argon2.verify(hash, data.password);
+            if(valid) {
+                data.password = await argon2.hash(req.body.passowrd);
+            }
+        }
+        data.role = req.body.role;
+    });
 };
