234103687fe6ea0017831cf91917d614fea333fa
alenjames2000 <alenjames@ufl.edu>
2020-02-29 17:30:49 -0500

    Basic service model

---
+++ api/model/service.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose';
+
+const Service = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: mongoose.Number
+    }
+    addinfo: {
+        type: String
+    }
+});
+
+export default mongoose.model('Service', Serivce);
