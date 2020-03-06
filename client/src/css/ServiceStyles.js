510b7815f6a702c0f21c420d14813cf7a7ce6c2b
LaurenNewman1 <lauren.newman@ufl.edu>
2020-03-06 14:04:23 -0500

    service card template

---
+++ client/src/css/ServiceStyles.js
@@ -0,0 +1,37 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: '100%',
+  },
+  media: {
+    height: 150,
+  },
+  flexContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  time: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  price: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  pricePaper: {
+    padding: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 5,
+    marginRight: 5,
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
+
+export default useStyles;
--- client/src/layouts/Services.jsx
+++ client/src/layouts/Services.jsx
@@ -1,10 +1,95 @@
 import React from 'react';
+import {
+  Typography, Card, CardMedia, CardContent, Grid, CardActions, Button, Paper,
+} from '@material-ui/core';
+import {
+  AccessTime,
+} from '@material-ui/icons';
+import {
+  useHistory,
+} from 'react-router-dom';
 import Page from '../components/Page';
+import useStyles from '../css/ServiceStyles';
+import hair from '../images/hair.jpeg';
 
 const Services = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
   return (
     <Page>
-      <div>Services</div>
+      <Typography variant="h4">Nails</Typography>
+      <Grid container spacing={3} className={classes.container}>
+        <Grid item xs={3}>
+          <Card>
+            <CardMedia component="image" image={hair} className={classes.media}>
+              <div className={classes.price}>
+                <Paper className={classes.pricePaper}>
+                  <Typography variant="body2">$30</Typography>
+                </Paper>
+              </div>
+            </CardMedia>
+            <CardContent>
+              <div className={classes.flexContainer}>
+                <Typography variant="subtitle1">Trim</Typography>
+                <Typography variant="subtitle2" className={classes.time}>
+                  <AccessTime />
+                  {' '}
+                  30 min
+                </Typography>
+              </div>
+              <Typography variant="body2" color="textSecondary" component="p">
+                This is a description of what this service is.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                onClick={() => history.push('/book')}
+              >
+                Book Now
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Card>
+            <CardMedia component="image" image={hair} className={classes.media}>
+              <div className={classes.price}>
+                <Paper className={classes.pricePaper}>
+                  <Typography variant="body2">$30</Typography>
+                </Paper>
+              </div>
+            </CardMedia>
+            <CardContent>
+              <div className={classes.flexContainer}>
+                <Typography variant="subtitle1">Trim</Typography>
+                <Typography variant="subtitle2" className={classes.time}>
+                  <AccessTime />
+                  {' '}
+                  30 min
+                </Typography>
+              </div>
+              <Typography variant="body2" color="textSecondary" component="p">
+                This is a description of what this service is.
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                onClick={() => history.push('/book')}
+              >
+                Book Now
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
     </Page>
   );
 };
