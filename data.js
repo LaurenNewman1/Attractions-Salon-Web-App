5db9d55b2d7374b6f76c4ee6ebef7797e70df20d
Christopher Miller <admin@mctherealm.net>
2020-03-06 14:52:10 -0500

    Added mock data

---
+++ data.js
@@ -0,0 +1,91 @@
+export default [
+  {
+    name: 'Single process color',
+    description: 'NEEDS_DESCRIPTION',
+    price: 55,
+    time: -1,
+    banner: 'NEED_IMAGE',
+    type: 'hair',
+    subtype: 'dye',
+    addons: [
+      {
+        name: 'Haircut',
+        price: 15,
+      },
+    ],
+  },
+  {
+    name: 'Relaxer',
+    description: 'NEEDS_DESCRIPTION',
+    price: 65,
+    time: -1,
+    banner: 'NEED_IMAGE',
+    type: 'hair',
+    subtype: 'treatments',
+    addons: [
+      {
+        name: 'Haircut',
+        price: 10,
+      },
+      {
+        name: 'Color Rinse',
+        price: 25,
+      },
+    ],
+  },
+  {
+    name: 'Haircut',
+    description: 'NEEDS_DESCRIPTION',
+    price: 30,
+    time: -1,
+    banner: 'NEED_IMAGE',
+    type: 'hair',
+    subtype: 'haircuts',
+    addons: [
+      {
+        name: 'Blow dry',
+        price: 8,
+      },
+    ],
+  },
+  {
+    name: 'Shampoo',
+    description: 'NEEDS_DESCRIPTION',
+    price: 20,
+    time: -1,
+    banner: 'NEED_IMAGE',
+    type: 'hair',
+    subtype: 'wash',
+    addons: [
+      {
+        name: 'Blow dry',
+        price: 10,
+      },
+    ],
+  },
+  {
+    name: 'Spiral Sets',
+    description: 'NEEDS_DESCRIPTION',
+    price: 50,
+    time: -1,
+    banner: 'NEED_IMAGE',
+    type: 'hair',
+    subtype: 'styling',
+  },
+  {
+    name: 'Lip',
+    description: 'NEEDS_DESCRIPTION',
+    price: 6,
+    time: -1,
+    banner: 'NEED_IMAGE',
+    type: 'wax',
+  },
+  {
+    name: 'Manicure',
+    description: 'This is a mock up',
+    price: 15,
+    time: -1,
+    banner: 'NEED_IMAGE',
+    type: 'nail',
+  },
+];
