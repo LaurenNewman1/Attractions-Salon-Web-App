6c5e10104ad2676f8e76536d459103d020a2ca8c
Dhruv Patel <dhruv2000patel@gmail.com>
2020-03-10 17:36:36 -0400

    Appointments

--- client/src/components/Appointments.jsx
+++ client/src/components/Appointments.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
-import hairPic from '../curlyHair.jpg';
+import hairPic from '../images/loginImage.jpg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
