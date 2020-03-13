1b4c15fbae8153b6b1f451c4c8011079499e718d
alenjames2000 <alenjames@ufl.edu>
2020-03-13 14:25:35 -0400

    Find user by role

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,6 +1,7 @@
 import * as users from '../controllers/userController';
 import * as services from '../controllers/serviceController';
 import * as sessions from '../controllers/sessionController';
+import * as appointments from '../controllers/appointmentController';
 
 const router = (app) => {
   // Session Routes
@@ -9,6 +10,7 @@ const router = (app) => {
 
   // User Routes
   app.get('/api/users', users.read);
+  app.get('/api/users/roles/:role', users.readByRole)
   app.delete('/api/users/:someId', users.remove);
   app.put('/api/users/:someId', users.update);
   app.post('/api/users', users.create);
@@ -19,6 +21,13 @@ const router = (app) => {
   app.get('/api/services/types/:type', services.readType);
   app.get('/api/services/types/:type/:subtype', services.readType);
   app.get('/api/services/:someId', services.read);
+
+  // Appointment Routes
+  app.get('/api/appointments/:someId', appointments.read);
+  app.get('/api/appointments', appointments.readall);
+  app.delete('/api/appointments/:someId', appointments.remove);
+  app.put('/api/appointments/:someId', appointments.update);
+  app.post('/api/appointments', appointments.create);
 };
 
 export default router;
