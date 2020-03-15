13d3ffffb4cee15a6a1c4273f396fd13144f0da9
sillygoosetime <tallfin@protonmail.com>
2020-03-14 22:15:43 -0400

    Finish confirmation page layout

--- client/src/css/ConfirmationStyles.js
+++ client/src/css/ConfirmationStyles.js
@@ -1,7 +1,27 @@
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
-
+  page: {
+    height: 'calc(100vh - 64px)',
+    display: 'flex',
+    alignItems: 'center',
+    background: 'white',
+    color: 'black',
+    justifyContent: 'center',
+  },
+  centerCol: {
+    height: 'calc(100vh - 64px)',
+    background: '#fafafa',
+  },
+  text: {
+    fontSize: '28px',
+    textAlign: 'center',
+    padding: '10vh',
+  },
+  icon: {
+    fontSize: '300%',
+    color: '#00eb7d',
+  },
 }));
 
 
--- client/src/layouts/Confirmation.jsx
+++ client/src/layouts/Confirmation.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Button, ListItemIcon } from '@material-ui/core';
+import { CheckCircle } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
 import Page from '../components/Page';
 import useStyles from '../css/ConfirmationStyles';
 
 const Confirmation = () => {
   const classes = useStyles();
-
+  const history = useHistory();
   return (
     <Page>
       <Grid container className={classes.page}>
-        <div>Confirmation</div>
+        <Grid item className={classes.centerCol} xs={5}>
+
+          <div className={classes.text}>
+            <p>Appointment Confirmed!</p>
+            <CheckCircle className={classes.icon} />
+            <p>Please check your E-mail for confirmation and details.</p>
+            <p>Order #: 123456789</p>
+            <Button
+              onClick={() => history.push('/book')}
+              variant="contained"
+              color="primary"
+              size="small"
+            >
+              MAKE ANOTHER BOOKING
+            </Button>
+          </div>
+
+        </Grid>
       </Grid>
     </Page>
   );
